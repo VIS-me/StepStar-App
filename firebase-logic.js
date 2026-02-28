2d92939d846bf8a4da95ca50773a4180c2c46eaf
VIS-me <ivan3bov@gmail.com>
2026-02-28 12:28:54 +0100

    firebase-logic.js

--- firebase-logic.js
+++ firebase-logic.js
@@ -16,34 +16,29 @@ export async function syncUser(tgUser) {
         return userSnap.data();
     } else {
         const newUser = {
-    ...freshData,
-    economy: { balance: 0, total_earned: 0 },
-    inventory: { frames: ["white"] },
-    selected: { frame: "white" },
-    stats: { 
-        steps_today: 0, 
-        calories_today: 0, // Добавили
-        steps_total: 0 
-    },
-    settings: { is_vip: false, privacy_mode: "public" },
-    tournament: { is_registered: false }
-};
+            ...freshData,
+            economy: { balance: 0, total_earned: 0 },
+            inventory: { frames: ["white"] },
+            selected: { frame: "white" },
+            stats: { steps_today: 0, calories_today: 0, steps_total: 0 },
+            settings: { is_vip: false, privacy_mode: "public" },
+            tournament: { is_registered: false }
+        };
         await setDoc(userRef, newUser);
         return newUser;
     }
 }
 
-export async function buyVip(userId) {
+export async function buyVipInDb(userId) {
     const VIP_PRICE = 2500;
     const userRef = doc(db, "users", String(userId));
-    const userSnap = await getDoc(userRef);
     
-    if (userSnap.data().economy.balance < VIP_PRICE) return alert("Недостаточно монет!");
-
     await updateDoc(userRef, {
         "economy.balance": increment(-VIP_PRICE),
         "settings.is_vip": true,
+        "settings.privacy_mode": "private",
         "selected.frame": "gold_vip_frame",
         "inventory.frames": arrayUnion("gold_vip_frame")
     });
+    return true;
 }
